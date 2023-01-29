24e4edb31945914c232bf6e0859fb42073585f8b
RizzyFuzz <rizzowohq@gmail.com>
2023-01-29 21:30:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -63,5 +63,5 @@ app.use((req, res) => {
 
 //Configure App
 app.listen(PORT, () => {
-  console.log('Server running on http://localhost:'+PORT');
+  console.log('Server running on http://localhost:'+PORT);
 });
