7083391687412b06cd14e92f980f4a16f917eff5
RizzyFuzz <rizzowohq@gmail.com>
2023-01-30 11:55:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const firstApi = require("./lib/firstApi.js");
 const randomApi = require("./lib/randomApi.js");
-const cors = require('cors');
+
 
 const app = express();
 app.use(logger('dev'));
