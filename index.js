7c827e77180387cac2f6595e2c80ab0dd578e73c
RizzyFuzz <rizzowohq@gmail.com>
2023-01-31 22:52:40 +0800

    solid

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ const randomApi = require("./lib/randomApi.js");
 
 class e621Api {
   constructor({ clientUsername, clientApikey }) {
+    if (!clientUsername) {
+    throw new Error('Ur Username e621.net required!');
+  }
+    if (!clientApikey) {
+    throw new Error('Ur Apikey e621.net required!');
+  }
     this.clientUsername = clientUsername;
     this.clientApikey = clientApikey;
   }
 }
 
 e621Api.prototype.getAll = async function (tags) {
+  if (!tags) {
+    throw new Error('Tags/artist e621.net required!');
+  }
   return await firstApi(tags, this.clientUsername, this.clientApikey);
 };
 
 e621Api.prototype.getRandom = async function (tags) {
+  if (!tags) {
+    throw new Error('Tags/artist e621.net required!');
+  }
   return await randomApi(tags, this.clientUsername, this.clientApikey);
 };
 
