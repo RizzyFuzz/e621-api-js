50045cc1b6a05ea6ae003d81332a07a2c73935d8
RizzyFuzz <rizzowohq@gmail.com>
2023-01-31 22:08:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
 const firstApi = require("./lib/firstApi.js");
 const randomApi = require("./lib/randomApi.js");
 
-
 class e621Api {
   constructor({ clientUsername, clientApikey }) {
     this.clientUsername = clientUsername;
     this.clientApikey = clientApikey;
-    this.Display = Display
-  }
-
- async getAll(tags) {
-    return await firstApi(tags, this.clientUsername, this.clientApikey);
-  }
-
-  async getRandom(tags) {
-    return await  randomApi(tags, this.clientUsername, this.clientApikey);
-  }
-
-  Display() {
-    return {
-      version: this.version,
-      author: this.author,
-      get: {
-        getAll: this.getAll.bind(this),
-        getRandom: this.getRandom.bind(this),
-      },
-    };
   }
 }
 
+e621Api.prototype.getAll = function(tags) {
+  return firstApi(tags, this.clientUsername, this.clientApikey);
+};
+
+e621Api.prototype.getRandom = function(tags) {
+  return randomApi(tags, this.clientUsername, this.clientApikey);
+};
+
 module.exports = e621Api;
+
+ 
