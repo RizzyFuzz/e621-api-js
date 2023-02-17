ec0a3326136d995874de4563fb3220ff2dddb720
RizzyFuzz <rizzowohq@gmail.com>
2023-02-17 21:27:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -173,8 +173,7 @@ app.get("/api/getMedia", (req, res) => {
     accept: "*/*",
     Cookie:
       "_danbooru_session=1wdBGy%2Fr55LVn554a7gLc6rBVNoFSsjcZstcDhnaUWXFDiJL%2Bcv7XFLUySHKoTR9hBtTFzP%2FxdH29vomdWEGyuh6Dvy3xA0O5rZqGG0u8bxXY%2FHzH1f88V9qsI6r0qrIAteIatZC01t6%2Fxy6g2zDfXo3HxEY2jKai1zlWzN0ksTVxLtTWb6aP8GQDEuwF2hSwrnjQBWFpAgzezog%2Bl4tG58dSRfsvjjshwubFV1DQL8imJPpGqUe7LFNLnn85r9UyQ9UKaBiOz0hyKcrV6EOskWXh2cT7iAkBKjFuaXdLkynHPlZWrS6%2BChpOPKS6uoimSQ0Q13uxUabRRNEkmShFCiDK1fU--3EsX0dcp%2BscT4tJp--Gi1chQ7z3yM9xQEDo9Gh8w%3D%3D",
-    "Content-Type": "image/png",
-  };
+    };
 
   axios
     .get(url, {
@@ -182,9 +181,9 @@ app.get("/api/getMedia", (req, res) => {
       responseType: "arraybuffer",
     })
     .then((response) => {
-      const buffer = Buffer.from(response.data, "binary");
-      res.contentType("image/png");
-      res.send(buffer);
+      const contentType = response.headers['content-type'];
+      res.contentType(contentType);
+      res.send(Buffer.from(response.data, 'binary')); 
     })
     .catch((error) => {
       console.log(error);
